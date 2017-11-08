1b89fbe62ffe2ed34537a1b84cd80903ad738f6d
Wenjie Yang <wenjie.yang@ihandysoft.com>
2017-11-08 16:16:56 +0800

    add server api

--- app/db/mongodb.js
+++ app/db/mongodb.js
@@ -5,13 +5,21 @@ const db = mongoose.connect('mongodb://127.0.0.1:27017/acfun',  { useMongoClient
 const schema = new mongoose.Schema({});
 
 const News = mongoose.model('news', schema, 'news');
+//
+// exports.get_list = (data, callback) => {
+//     News.find({}, null, { skip: data.skip, limit: data.limit }, (err, response) => {
+//         !err && callback(response);
+//     })
+// }
+exports.get_list = (data, callback) => {
+    News.find({}).sort({create_time: -1}).skip(data.skip).limit(data.limit).exec((err, response) => {
+        !err && callback(response);
+    })
+}
 
-exports.get_list = (callback) => {
-    News.find({}, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            callback(data);
-        }
+
+exports.get_news_by_id = (id, callback) => {
+    News.find({ "_id": id }, (err, response) => {
+        !err && callback(response);
     })
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -11,9 +11,26 @@ module.exports = (app) => {
     app.use(bodyParser.json({limit: '50mb'}));
     app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 
-    app.get('/list', (req, res) => {
-        db.get_list((data) => {
-            res.json({data})
-        });
+    // app.get('/list', (req, res) => {
+    //     db.get_list((data) => {
+    //         res.json({data})
+    //     });
+    // });
+
+    app.post('/api/news_list', upload.array(), (req, res) => {
+        const data = req.body;
+        db.get_list(data, (response) => res.json({
+            data: response,
+            end: data.skip + data.limit
+        }));
+    });
+
+    app.get('/api/get_news_by_id', (req, res) => {
+        const id = req.param('id');
+        db.get_news_by_id(id, (response) => {
+            res.json({
+                data: response
+            })
+        })
     })
 }
